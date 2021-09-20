389544de0dfe5e80c540d9601a4d47b4846982af
Lui Habl <txaicalvoso@gmail.com>
2021-09-20 20:58:24 +0200

    Removing player sprites

--- src/composer.cpp
+++ src/composer.cpp
@@ -63,21 +63,7 @@ Entity * Composer::create_player(Scene * scene, std::string name, const TinySDL:
     entity->add_component(Player());
 
     
-    auto * animator = entity->add_component(AnimatedSprite());
-    
-    auto * walk_animation = animator->add("walk");
-    float walk_delay = 90.0f;
-    walk_animation->frames.push_back({TexRegion(Content::find<Texture>("sprites/player2"), Rect(0, 0, 32, 32)), {16, 16}, walk_delay});
-    walk_animation->frames.push_back({TexRegion(Content::find<Texture>("sprites/player3"), Rect(0, 0, 32, 32)), {16, 16}, walk_delay});
-    walk_animation->frames.push_back({TexRegion(Content::find<Texture>("sprites/player4"), Rect(0, 0, 32, 32)), {16, 16}, walk_delay});
-    walk_animation->frames.push_back({TexRegion(Content::find<Texture>("sprites/player5"), Rect(0, 0, 32, 32)), {16, 16}, walk_delay});
-
-    auto * idle_animation = animator->add("idle");
-    idle_animation->frames.push_back({TexRegion(Content::find<Texture>("sprites/player1"), Rect(0, 0, 32, 32)), {16, 16}, 150.0f});
-
-    auto * jump_animation = animator->add("jump");
-    jump_animation->frames.push_back({TexRegion(Content::find<Texture>("sprites/player6"), Rect(0, 0, 32, 32)), {16, 16}, 150.0f});
-
+    auto * animator = entity->add_component(AnimatedSprite(name));
     animator->play("idle");
 
     auto * collider = entity->add_component(Collider({-7, -1, 14, 17}));
@@ -85,7 +71,6 @@ Entity * Composer::create_player(Scene * scene, std::string name, const TinySDL:
     auto * actor = entity->add_component(Actor());
     actor->collider = collider;
 
-  
     return entity;
 }
 
